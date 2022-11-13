6307e2f1d74f49c1ffa0e3a62563d1e2d8ca2083
Vytautas Dirzauskas <vytautas@kilo.lt>
2022-11-13 19:17:58 +0200

    fixes

--- admin/qtx_admin_gutenberg.php
+++ admin/qtx_admin_gutenberg.php
@@ -4,7 +4,7 @@
  * @author: herrvigg
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit;
 }
 
@@ -14,37 +14,43 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Manages the Gutenberg block editor with the related REST API.
  * Limitation: only the single language mode is supported.
  */
-class QTX_Admin_Gutenberg {
+class QTX_Admin_Gutenberg
+{
+
+    const WP_BLOCK_POST_TYPE = 'wp_block';
+
     /**
      * QTX_Admin_Gutenberg constructor
      */
-    public function __construct() {
-        add_action( 'rest_api_init', array( $this, 'rest_api_init' ) );
-        add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_block_editor_assets' ) );
+    public function __construct()
+    {
+        add_action('rest_api_init', array ($this, 'rest_api_init'));
+        add_action('enqueue_block_editor_assets', array ($this, 'enqueue_block_editor_assets'));
     }
 
     /**
      * Register the REST filters
      */
-    public function rest_api_init() {
+    public function rest_api_init()
+    {
         global $q_config;
 
         // Filter to allow qTranslate-XT to manage the block editor (single language mode)
-        $admin_block_editor = apply_filters( 'qtranslate_admin_block_editor', true );
-        if ( ! $admin_block_editor ) {
+        $admin_block_editor = apply_filters('qtranslate_admin_block_editor', true);
+        if (!$admin_block_editor) {
             return;
         }
 
-        $post_types = get_post_types( array( 'show_in_rest' => true ) );
-        foreach ( $post_types as $post_type ) {
-            $post_type_excluded = isset( $q_config['post_type_excluded'] ) && in_array( $post_type, $q_config['post_type_excluded'] );
-            if ( ! $post_type_excluded ) {
-                add_filter( "rest_prepare_{$post_type}", array( $this, 'rest_prepare' ), 99, 3 );
+        $post_types = get_post_types(array ('show_in_rest' => true));
+        foreach ($post_types as $post_type) {
+            $post_type_excluded = isset($q_config['post_type_excluded']) && in_array($post_type, $q_config['post_type_excluded']);
+            if (!$post_type_excluded) {
+                add_filter("rest_prepare_{$post_type}", array ($this, 'rest_prepare'), 99, 3);
             }
         }
 
-        add_filter( 'rest_request_before_callbacks', array( $this, 'rest_request_before_callbacks' ), 99, 3 );
-        add_filter( 'rest_request_after_callbacks', array( $this, 'rest_request_after_callbacks' ), 99, 3 );
+        add_filter('rest_request_before_callbacks', array ($this, 'rest_request_before_callbacks'), 99, 3);
+        add_filter('rest_request_after_callbacks', array ($this, 'rest_request_after_callbacks'), 99, 3);
     }
 
     /**
@@ -58,26 +64,39 @@ class QTX_Admin_Gutenberg {
      *
      * @return mixed
      */
-    public function rest_prepare( $response, $post, $request ) {
+    public function rest_prepare($response, $post, $request)
+    {
         global $q_config;
 
-        if ( $request->get_param( 'context' ) !== 'edit' || $request->get_method() !== 'GET' ) {
+        if ($request->get_param('context') !== 'edit' || $request->get_method() !== 'GET') {
             return $response;
         }
 
         // See https://github.com/WordPress/gutenberg/issues/14012#issuecomment-467015362
-        require_once( ABSPATH . 'wp-admin/includes/post.php' );
+        require_once(ABSPATH . 'wp-admin/includes/post.php');
 
-        if ( ! use_block_editor_for_post( $post ) ) {
+        if (!use_block_editor_for_post($post)) {
             return $response;
         }
 
-        assert( ! $q_config['url_info']['doing_front_end'] );
+        assert(!$q_config['url_info']['doing_front_end']);
 
         // TODO allow user to select editor lang with buttons
         $editor_lang = $q_config['url_info']['language'];
 
-        $response = $this->select_raw_response_language( $response, $editor_lang );
+        if (wp_is_json_request()) {
+            $url_params = [];
+
+            if (isset($_SERVER['HTTP_REFERER'])) {
+                parse_str($_SERVER['HTTP_REFERER'], $url_params);
+
+                if (isset($url_params['lang'])) {
+                    $editor_lang = $url_params['lang'];
+                }
+            }
+        }
+
+        $response = $this->select_raw_response_language($response, $editor_lang);
 
         return $response;
     }
@@ -91,38 +110,52 @@ class QTX_Admin_Gutenberg {
      *
      * @return mixed
      */
-    public function rest_request_before_callbacks( $response, $handler, $request ) {
-        if ( $request->get_method() !== 'PUT' && $request->get_method() !== 'POST' ) {
+    public function rest_request_before_callbacks($response, $handler, $request)
+    {
+        if ($request->get_method() !== 'PUT' && $request->get_method() !== 'POST') {
             return $response;
         }
 
-        $editor_lang = $request->get_param( 'qtx_editor_lang' );
-        if ( ! isset( $editor_lang ) ) {
+        $editor_lang = $request->get_param('qtx_editor_lang');
+        $post_id = $request->get_param('id');
+
+        if (!isset($post_id)) {
             return $response;
         }
 
-        $request_body = json_decode( $request->get_body(), true );
-        $post         = get_post( $request->get_param( 'id' ), ARRAY_A );
+        $post = get_post($request->get_param('id'), ARRAY_A);
 
-        $fields = [ 'title', 'content', 'excerpt' ];
-        foreach ( $fields as $field ) {
-            if ( ! isset( $request_body[ $field ] ) ) {
+        if ($post['post_type'] === self::WP_BLOCK_POST_TYPE && !isset($editor_lang)) {
+            global $q_config;
+            $editor_lang = $q_config['url_info']['language'];
+        }
+
+        if (!isset($editor_lang)) {
+            return $response;
+        }
+
+        $request_body = json_decode($request->get_body(), true);
+        $post = get_post($request->get_param('id'), ARRAY_A);
+
+        $fields = ['title', 'content', 'excerpt'];
+        foreach ($fields as $field) {
+            if (!isset($request_body[$field])) {
                 continue; // only the changed fields are set in the REST request
             }
 
             // split original values with empty strings by default
-            $original_value = $post[ 'post_' . $field ];
-            $split          = qtranxf_split( $original_value );
+            $original_value = $post['post_' . $field];
+            $split = qtranxf_split($original_value);
 
             // replace current language with the new value
-            $split[ $editor_lang ] = $request_body[ $field ];
+            $split[$editor_lang] = $request_body[$field];
 
             // remove auto-draft default title for other languages (not the correct translation)
-            if ( $field === 'title' && $post['post_status'] === 'auto-draft' ) {
+            if ($field === 'title' && $post['post_status'] === 'auto-draft') {
                 global $q_config;
-                foreach ( $q_config['enabled_languages'] as $lang ) {
-                    if ( $lang !== $editor_lang ) {
-                        $split[ $lang ] = '';
+                foreach ($q_config['enabled_languages'] as $lang) {
+                    if ($lang !== $editor_lang) {
+                        $split[$lang] = '';
                     }
                 }
             }
@@ -131,9 +164,9 @@ class QTX_Admin_Gutenberg {
             //$sep = '[';
             //$new_data = qtranxf_collect_translations_deep( $split, $sep );
             //$new_data = qtranxf_join_texts( $split, $sep );
-            $new_data = qtranxf_join_b( $split );
+            $new_data = qtranxf_join_b($split);
 
-            $request->set_param( $field, $new_data );
+            $request->set_param($field, $new_data);
         }
 
         return $response;
@@ -148,17 +181,18 @@ class QTX_Admin_Gutenberg {
      *
      * @return mixed
      */
-    public function rest_request_after_callbacks( $response, $handler, $request ) {
-        if ( $request->get_param( 'context' ) !== 'edit' || $request->get_method() !== 'PUT' && $request->get_method() !== 'POST' ) {
+    public function rest_request_after_callbacks($response, $handler, $request)
+    {
+        if ($request->get_param('context') !== 'edit' || $request->get_method() !== 'PUT' && $request->get_method() !== 'POST') {
             return $response;
         }
 
-        $editor_lang = $request->get_param( 'qtx_editor_lang' );
-        if ( ! isset( $editor_lang ) ) {
+        $editor_lang = $request->get_param('qtx_editor_lang');
+        if (!isset($editor_lang)) {
             return $response;
         }
 
-        $response = $this->select_raw_response_language( $response, $editor_lang );
+        $response = $this->select_raw_response_language($response, $editor_lang);
 
         return $response;
     }
@@ -166,26 +200,27 @@ class QTX_Admin_Gutenberg {
     /**
      * Enqueue the JS script
      */
-    public function enqueue_block_editor_assets() {
+    public function enqueue_block_editor_assets()
+    {
         // By default, excluded post types are filtered out.
         global $q_config;
         $post_type = qtranxf_post_type();
-        $post_type_excluded = isset( $q_config['post_type_excluded'] ) && isset( $post_type ) && in_array( $post_type, $q_config['post_type_excluded'] );
-        
+        $post_type_excluded = isset($q_config['post_type_excluded']) && isset($post_type) && in_array($post_type, $q_config['post_type_excluded']);
+
         // Filter to allow qTranslate-XT to manage the block editor (single language mode)
-        $admin_block_editor = apply_filters( 'qtranslate_admin_block_editor', ! $post_type_excluded );
-        if ( ! $admin_block_editor ) {
+        $admin_block_editor = apply_filters('qtranslate_admin_block_editor', !$post_type_excluded);
+        if (!$admin_block_editor) {
             return;
         }
 
         wp_register_script(
             'qtx-gutenberg',
-            plugins_url( 'dist/editor-gutenberg.js', QTRANSLATE_FILE ),
-            array(),
+            plugins_url('dist/editor-gutenberg.js', QTRANSLATE_FILE),
+            array (),
             QTX_VERSION,
             true
         );
-        wp_enqueue_script( 'qtx-gutenberg' );
+        wp_enqueue_script('qtx-gutenberg');
     }
 
     /**
@@ -196,18 +231,19 @@ class QTX_Admin_Gutenberg {
      *
      * @return mixed
      */
-    private function select_raw_response_language( $response, $editor_lang ) {
+    private function select_raw_response_language($response, $editor_lang)
+    {
         $response_data = $response->get_data();
-        if ( isset( $response_data['content'] ) && is_array( $response_data['content'] ) && isset( $response_data['content']['raw'] ) ) {
-            $response_data['title']['raw']   = qtranxf_use( $editor_lang, $response_data['title']['raw'], false, true );
-            $response_data['content']['raw'] = qtranxf_use( $editor_lang, $response_data['content']['raw'], false, true );
+        if (isset($response_data['content']) && is_array($response_data['content']) && isset($response_data['content']['raw'])) {
+            $response_data['title']['raw'] = qtranxf_use($editor_lang, $response_data['title']['raw'], false, true);
+            $response_data['content']['raw'] = qtranxf_use($editor_lang, $response_data['content']['raw'], false, true);
 
-            if ( isset( $response_data['excerpt']['raw'] ) ) {
-                $response_data['excerpt']['raw'] = qtranxf_use( $editor_lang, $response_data['excerpt']['raw'], false, true );
+            if (isset($response_data['excerpt']['raw'])) {
+                $response_data['excerpt']['raw'] = qtranxf_use($editor_lang, $response_data['excerpt']['raw'], false, true);
             }
 
             $response_data['qtx_editor_lang'] = $editor_lang;
-            $response->set_data( $response_data );
+            $response->set_data($response_data);
         }
 
         return $response;
